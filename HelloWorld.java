1a101ea9a63e572030230757cc2187bd813e77cf
Đào Anh Huy <43365727+HuyKnife@users.noreply.github.com>
2018-09-24 22:35:39 +0700

    print hello world this is tdtu before print user name

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class HelloWorld{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input name: \n")
-        System.out.println("Hello World I am " + sc.nextLine());
         System.out.print("Hello World This is TDTU");
+        System.out.print("Input name: \n");
+        System.out.println("Hello World I am " + sc.nextLine());
     }
 }
