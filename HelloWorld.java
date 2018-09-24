a1f3ca67ac10c66170d441bcf960cdb11b5ca51d
Đào Anh Huy <43365727+HuyKnife@users.noreply.github.com>
2018-09-24 22:24:07 +0700

    change I am TDTU to This is TDTU

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
     public static void main(String[] args) {
-        System.out.print("Hello World I am TDTU");
+        System.out.print("Hello World This is TDTU");
     }
 }
